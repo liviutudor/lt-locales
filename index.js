160f5e556dff9b07e669acb4a8059d9f6836d606
Liviu Tudor <liviu.tudor@gmail.com>
2018-03-12 23:44:37 -0700

    made it work

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-class Locales {
+let ISO3166_1 = null;
+let ISO639_1 = null;
 
-}
+loadData = () => {
+    if (ISO3166_1 && ISO639_1) return;
+    ISO3166_1 = require('./data/ISO3166_1.json');
+    ISO639_1 = require('./data/ISO639_1.json');
+};
 
-module.exports = Locales;
+parseLocale = (locale) => {
+    loadData();
+    let [languageCode, scriptCode, countryCode] = locale.split('-');
+    if (!countryCode) {
+        countryCode = scriptCode;
+        scriptCode = null;
+    } else {
+        languageCode = `${languageCode}-${scriptCode}`;
+    }
+    let language;
+    let country;
 
+    if (!countryCode) {
+        language = ISO639_1[languageCode];
+        if (!language) {
+            country = ISO3166_1[languageCode] || languageCode;
+            countryCode = languageCode;
+            languageCode = undefined;
+        }
+    } else {
+        language = ISO639_1[languageCode] || languageCode;
+        country = ISO3166_1[countryCode] || countryCode;
+    }
+
+    return {
+        languageCode,
+        language,
+        countryCode,
+        country,
+        locale,
+    };
+};
+
+compareByLangCode = (locale1, locale2) => {
+    if (!locale1) return -1;
+    if (!locale2) return 1;
+    if (locale1.languageCode < locale2.languageCode) return -1;
+    if (locale1.languageCode === locale2.languageCode) return 0;
+    return 1;
+};
+
+compareByCountryCode = (locale1, locale2) => {
+    if (!locale1) return -1;
+    if (!locale2) return 1;
+    if (locale1.countryCode < locale2.countryCode) return -1;
+    if (locale1.countryCode === locale2.countryCode) return 0;
+    return 1;
+};
+
+
+module.exports = {
+    parseLocale,
+    compareByLangCode,
+    compareByCountryCode
+};
--- test.js
+++ test.js
@@ -1,7 +1,62 @@
 const Locales = require('./index.js');
 
 describe('Locales', () => {
-	it('initializes correctly', () => {
-		expect(Locales).not.toBe(null);
-	});
+    it('initializes correctly', () => {
+        expect(Locales).not.toBe(null);
+    });
+
+    it('parseLocale simple', () => {
+        const res = Locales.parseLocale('en-US');
+        expect(res).toEqual({
+            country: 'UNITED STATES',
+            countryCode: 'US',
+            language: 'English',
+            languageCode: 'en',
+            locale: 'en-US'
+        });
+    });
+
+    it('parseLocale chinese', () => {
+        const res = Locales.parseLocale('zh-Hant-TW');
+        expect(res).toEqual({
+            country: 'TAIWAN, PROVINCE OF CHINA',
+            countryCode: 'TW',
+            language: 'Traditional Chinese',
+            languageCode: 'zh-Hant',
+            locale: 'zh-Hant-TW'
+        });
+    });
+
+    it('parseLocale edge cases', () => {
+        let res;
+        res = Locales.parseLocale('ac-AB');
+        expect(res).toEqual({country: 'AB', countryCode: 'AB', language: 'ac', languageCode: 'ac', locale: 'ac-AB'});
+
+        res = Locales.parseLocale('en');
+        expect(res).toEqual({language: 'English', languageCode: 'en', locale: 'en'});
+
+        res = Locales.parseLocale('US');
+        expect(res).toEqual({country: 'UNITED STATES', countryCode: 'US', locale: 'US'});
+
+        res = Locales.parseLocale('xy');
+        expect(res).toEqual({country: 'xy', countryCode: 'xy', locale: 'xy'});
+    });
+
+    it('compareLangByCode compares 2 locales based on language code', () => {
+        expect(Locales.compareByLangCode(null, null)).toBe(-1);
+        expect(Locales.compareByLangCode({}, null)).toBe(1);
+        expect(Locales.compareByLangCode({}, {})).toBe(0);
+        expect(Locales.compareByLangCode({languageCode: 'a'}, {languageCode: 'b'})).toBe(-1);
+        expect(Locales.compareByLangCode({languageCode: 'b'}, {languageCode: 'a'})).toBe(1);
+        expect(Locales.compareByLangCode({languageCode: 'b'}, {languageCode: 'b'})).toBe(0);
+    });
+
+    it('compareLangByCountryCode compares 2 locales based on country code', () => {
+        expect(Locales.compareByCountryCode(null, null)).toBe(-1);
+        expect(Locales.compareByCountryCode({}, null)).toBe(1);
+        expect(Locales.compareByCountryCode({}, {})).toBe(0);
+        expect(Locales.compareByCountryCode({countryCode: 'a'}, {countryCode: 'b'})).toBe(-1);
+        expect(Locales.compareByCountryCode({countryCode: 'b'}, {countryCode: 'a'})).toBe(1);
+        expect(Locales.compareByCountryCode({countryCode: 'b'}, {countryCode: 'b'})).toBe(0);
+    });
 });
